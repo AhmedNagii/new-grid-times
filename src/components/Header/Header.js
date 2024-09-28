92d26fc58bffbd3bb8c0b7eb5dd1954376d909c9
AhmedNagii <ahmednagihassan09@gmail.com>
2024-09-28 12:59:43 +0300

    EX-header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import { Menu, Search, User } from 'react-feather';
+import React from "react";
+import styled from "styled-components/macro";
+import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from '../../constants';
+import { COLORS, QUERIES, WEIGHTS } from "../../constants";
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
 
 const Header = () => {
   return (
-    <header>
+    <HeaderCon>
       <SuperHeader>
         <Row>
           <ActionGroup>
@@ -29,16 +29,35 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <ActionGroupDeskTop>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </ActionGroupDeskTop>
         <Logo />
+        <SubscribeWrapper>
+          <Button>
+          Subscribe
+          </Button>  
+          <SubLink herf="/">Already a subscriber</SubLink>
+        
+        </SubscribeWrapper>
       </MainHeader>
-    </header>
+    </HeaderCon>
   );
 };
 
+const HeaderCon = styled.header``;
 const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
-  color: white;
+  color: ${COLORS.white};
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -51,11 +70,17 @@ const ActionGroup = styled.div`
   gap: 24px;
 
   /*
-    FIX: Remove the inline spacing that comes with
-    react-feather icons.
+  FIX: Remove the inline spacing that comes with
+  react-feather icons.
   */
   svg {
     display: block;
+  }
+`;
+const ActionGroupDeskTop = styled(ActionGroup)`
+  display: none;
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
   }
 `;
 
@@ -65,6 +90,40 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+  @media ${QUERIES.tabletAndUp} {
+    margin-top: 48px;
+    margin-bottom: 72px;
+  }
+  @media ${QUERIES.laptopAndUp} {
+    display: grid;
+    grid-template-columns: 1fr auto 1fr;
+    align-items: center;
+    justify-content: revert;
+    justify-items: start;
+    margin-top: 16px;
+    margin-bottom: 72px;
+  }
+`;
+const SubLink = styled.a`
+
+font-size: 0.875rem;
+color: var(--color-gray-900);
+font-style: italic;
+text-decoration: underline;
+`;
+const SubscribeWrapper = styled.div`
+  display: none;
+  
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+    justify-self: end;
+    align-self: end;
+    flex-direction: column;
+    align-items: center;
+    gap: 8px;
+
+  }
 `;
 
 export default Header;
